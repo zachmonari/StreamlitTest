285998888832442242c467980aa821e4a7eacc6b
zach <zachmonari002@gmail.com>
2025-10-28 12:09:25 +0300

    Added the audio elements

--- Fav.py
+++ Fav.py
@@ -2,4 +2,9 @@ import streamlit as st
 st.image("ZachTechs.jpg")
 st.title('My Favorite Person, Candy')
 
-
+st.write("A song for you when you feel down")
+st.audio("Close.mp3", format="audio/wav", loop=False)
+st.write("Favorite song for us")
+st.audio("PerfectDesign.mp3", format="audio/wav", loop=False)
+st.write("Favorite song by Ed Sheeran")
+st.audio("PerfectEd.mp3", format="audio/wav", loop=False)
