eac7ffba53da4c8aa6160584f57311174cdd4b53
zach <zachmonari002@gmail.com>
2026-01-08 08:51:18 +0300

    audio

--- BD.py
+++ BD.py
@@ -13,3 +13,18 @@ if click:
     # ---- Header ----
     st.title("🌸 Happy Birthday, My Love 💖")
     st.subheader("To the girl who makes every day feel like a celebration of love.")
+
+    col1, col2 ,col3= st.columns(3, vertical_alignment="bottom", border=False)
+    st.write("Songs for your birthday...")
+    with col1:
+        st.audio("Wadosi.mp3", format="audio/wav", loop=False)
+        image = Image.open("PH2.jpg")
+        st.image(image, caption="My amazing bbg💞", width=325)
+    with col2:
+        st.audio("Busy.mp3", format="audio/wav", loop=False)
+        image = Image.open("PH1.jpg")
+        st.image(image, caption="One of the beautiful moments 💞", width=325)
+    with col3:
+        st.audio("Harmonize.mp3", format="audio/wav", loop=False)
+        image = Image.open("PH2.jpg")
+        st.image(image, caption="My beautiful reason to smile 💞", width=325)
