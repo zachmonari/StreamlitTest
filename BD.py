46b4fe9b03b3c11147ed0aa642dc1ae20fa24ad8
zach <zachmonari002@gmail.com>
2026-01-08 08:19:01 +0300

    intro

--- BD.py
+++ BD.py
@@ -3,3 +3,13 @@ import PIL.Image as Image
 
 logo=Image.open("ZachTechs.jpg")
 st.image(logo,width=150)
+
+st.header("SURPRISE!")
+click=st.button("Click here")
+if click:
+    st.balloons()
+    st.markdown("Here's a bouquet for you bbg &mdash;\
+                :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+    # ---- Header ----
+    st.title("🌸 Happy Birthday, My Love 💖")
+    st.subheader("To the girl who makes every day feel like a celebration of love.")
